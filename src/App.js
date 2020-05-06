317845b81bec5d0c7b702764ae30301717a6b03d
sirekpiotr <sirekpiotr@icloud.com>
2020-05-06 18:13:42 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { useFetch } from './Hooks/useFetch'
+
+import Navbar from "./Components/Navbar"
+import Photos from "./Components/Photos"
+import Header from "./Components/Header"
+import PageController from "./Components/PageController"
+
+import styled from 'styled-components'
+
+const StyledWrapper = styled.div`
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    width: 100vw;
+    min-height: 100vh;
+
+    background-color: ${props => props.darkMode ? "#000" : "#fff"};
+    color: ${props => props.darkMode ? "#fff" : "#000"};
+`
 
 function App() {
+  const getFormattedSearchForURL = (text) => { return text.split(' ').join('+') }
+  const handleSearch = (newSearch) => { setSearch(newSearch) }
+  const handleDarkMode = () => setDarkMode( !darkMode )
+
+  const [page, setPage] = useState(1)
+  const [search, setSearch] = useState('')
+  const [darkMode, setDarkMode] = useState(false)
+
+  const { data, loading, error } = useFetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_KEY}&q=${getFormattedSearchForURL(search)}&image_type=photo&per_page=21&page=${page}`) 
+
+  useEffect(() => {
+    setPage(1)
+  }, [search])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <StyledWrapper darkMode={darkMode}>
+      <Navbar handleSearch={handleSearch} darkModeStatus={darkMode} handleDarkMode={handleDarkMode}/>
+      <Header actualUserSearch={search} isError={error}/>
+
+      <Photos 
+        data={data} 
+        loading={loading} 
+      />
+
+      <PageController 
+        nextPageAction={() => {setPage(page + 1)}}
+        prevPageAction={() => {page === 1 ? setPage(1) : setPage(page - 1)}} 
+        actualPage={page}       
+      />
+    </StyledWrapper>
+  )
 }
 
 export default App;
